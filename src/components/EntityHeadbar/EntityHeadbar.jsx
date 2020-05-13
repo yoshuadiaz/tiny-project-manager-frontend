dd09fa22f80c5987060fab6a347aab60bf33b898
Yoshua Díaz <yoshua.sdiaz@gmail.com>
2020-05-13 01:44:39 -0500

    Add EntityHeadbar component

---
+++ src/components/EntityHeadbar/EntityHeadbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './EntityHeadbar.css'
+const EntityHeadbar = props => {
+  return (
+    <div className='entityHeadbar'>
+      <div className='entityHeadbar_title'>
+        <h2>{props.title}</h2>
+      </div>
+      <div className='entityHeadbar_actions'>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default EntityHeadbar
